a39d2761f11aa5bcd7ce8697d1b63578bb99a1fa
dariusrisky <riskyramdani8fno26@gmail.com>
2025-08-03 23:19:53 +0700

    BACK TO BASIC KARENA CHAOS

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
 
 const corsOptions = {
-  origin: `${process.env.FRONTEND_URL}`,
+  origin: `http://localhost:5173`,
   credentials: true,
 };
 
