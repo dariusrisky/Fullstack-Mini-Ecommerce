eeb66567892a755e7abece8802ddeda4dd5ba58b
dariusrisky <riskyramdani8fno26@gmail.com>
2025-08-03 20:44:32 +0700

    gas

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: `${process.env.VITE_API_URL}/api/v1/user`,
+  baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
   headers: {
     "Content-Type": "application/json",
   },
